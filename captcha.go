9fa8f184a5b3fea14c170f66e09de4d25381fcf9
Suwitpong Kongmee <suwitpongk@gmail.com>
2018-12-17 14:42:44 +0700

    Add case OneOneOneOne

--- captcha.go
+++ captcha.go
@@ -1,1 +1,5 @@
 package captcha
+
+func Captcha(pattern int, leftOper int, oper int, rightOper int) string {
+	return "1 + one"
+}
--- captcha_test.go
+++ captcha_test.go
@@ -2,5 +2,13 @@ package captcha_test
 
 import (
 	"testing"
+	captcha "github.com/suwitpongk/captcha"
 )
 
+func TestCaseOneOneOneOne(t *testing.T) {
+	result := captcha.Captcha(1, 1, 1, 1)
+	expect := "1 + one"
+	if result != expect {
+		t.Error("it should be " , expect)
+	}
+}
