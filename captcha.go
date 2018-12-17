a6ecfdc55353ea790f1346c9841eb29f84e3b162
Suwitpong Kongmee <suwitpongk@gmail.com>
2018-12-17 14:56:36 +0700

    Refacter add convert number to struing character

--- captcha.go
+++ captcha.go
@@ -1,11 +1,40 @@
 package captcha
 
 func Captcha(pattern int, leftOper int, oper int, rightOper int) string {
-	if rightOper == 3{
-		return "1 + three"
+	return "1 + "+ConvertNumberToStringCharacter(rightOper)
+}
+
+func ConvertNumberToStringCharacter(number int) string {
+	if number == 0 {
+		return "zero"
 	}
-	if rightOper == 2{
-		return "1 + two"
+	if number == 1 {
+		return "one"
 	}
-	return "1 + one"
+	if number == 2 {
+		return "two"
+	}
+	if number == 3 {
+		return "three"
+	}
+	if number == 4 {
+		return "four"
+	}
+	if number == 5 {
+		return "five"
+	}
+	if number == 6 {
+		return "six"
+	}
+	if number == 7 {
+		return "seven"
+	}
+	if number == 8 {
+		return "eight"
+	}
+	if number == 9 {
+		return "nine"
+	}
+
+	return ""
 }
